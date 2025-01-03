460555406040ac2b366597aa404900b66b3819aa
Gerson Dias <gerson.dias@quotationfactory.com>
2025-01-03 14:52:39 -0300

    adds supertab

--- nvim/init.lua
+++ nvim/init.lua
@@ -636,6 +636,13 @@ require("lazy").setup({
 			local luasnip = require("luasnip")
 			luasnip.config.setup({})
 
+			local has_words_before = function()
+				unpack = unpack or table.unpack
+				local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+				return col ~= 0
+					and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+			end
+
 			cmp.setup({
 				snippet = {
 					expand = function(args)
@@ -684,6 +691,32 @@ require("lazy").setup({
 					["<C-h>"] = cmp.mapping(function()
 						if luasnip.locally_jumpable(-1) then
 							luasnip.jump(-1)
+						end
+					end, { "i", "s" }),
+					["<Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							-- You could replace select_next_item() with confirm({ select = true }) to get VS Code autocompletion behavior
+							-- cmp.select_next_item()
+							cmp.confirm({ selected = true })
+						elseif vim.snippet.active({ direction = 1 }) then
+							vim.schedule(function()
+								vim.snippet.jump(1)
+							end)
+						elseif has_words_before() then
+							cmp.complete()
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
+					["<S-Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_prev_item()
+						elseif vim.snippet.active({ direction = -1 }) then
+							vim.schedule(function()
+								vim.snippet.jump(-1)
+							end)
+						else
+							fallback()
 						end
 					end, { "i", "s" }),
 
